a78f8992c0ee8e6d5808c04918e225e9d0cdcfbf
Momen <momena881@gmail.com>
2025-09-14 23:31:43 +0300

    Side Nav booking

--- ITIResturant/ITIResturant/obj/Debug/net9.0/ITIResturant.AssemblyInfo.cs
+++ ITIResturant/ITIResturant/obj/Debug/net9.0/ITIResturant.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITIResturant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3973a254efb7611d2f42115847b1fcf1b075bda")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f221377254a49180aefc59c43c8be63365f9c9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITIResturant")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITIResturant")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
