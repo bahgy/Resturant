a6cad14fba6dac62a16dbefe940abb695860ade2
Menna <mennakhaledzoghla@gmail.com>
2025-09-15 21:51:00 +0300

    create EmailNotification and Feedback

--- ITIResturant/DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
+++ ITIResturant/DAL/obj/Debug/net9.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2741f4f7e400ba06a1df8df8f8725676b62a0357")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac2539fbf2a0cef156caa5937aa56dd8e670422e")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
